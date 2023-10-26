aee8c40e9f2fdd41a7759786ad81ca3bac0e748f
hotgorod <hotgorod@gmail.com>
2023-10-26 14:37:54 +0100

    persist removed

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import { App } from 'components/App';
 import './index.css';
 
 import { Provider } from 'react-redux';
-import {persistor, store} from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { devToolsEnhancer } from '@redux-devtools/extension';
 import contactReducer from './contactSlice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
+
  
 
 const enhancer = devToolsEnhancer();
 
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  whitelist: ['contacts']
-};
 
 
 
 export const store = configureStore(
   {
     reducer: {
-      contacts: persistReducer(contactsPersistConfig, contactReducer),
+      contacts: contactReducer,
     },
-    middleware: getDefaultMiddleware =>
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }),
+    
   },
   enhancer
 );
-export const persistor = persistStore(store);
