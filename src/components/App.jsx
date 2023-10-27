d9f28b879fb56f55e252ce61c57b5e1bfcd8ceaf
hotgorod <hotgorod@gmail.com>
2023-10-27 14:40:57 +0100

    contacts route added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 import ContactForm from './ContactForm/ContactForm.';
 import css from './App.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/contactSlice';
+import { Route, Routes } from 'react-router-dom';
 
-
+const ContactsPage = lazy(() => import('./pages/ContactsPage'));
 
 export const App = () => {
   
@@ -25,14 +26,25 @@ export const App = () => {
     dispatch(addContact({contactName, contactNumber}));
   }
    return (
-    <div className={css.container}>
-      <h1>Phonebook</h1>
-      <ContactForm addContactCallback={addContactToPhonebook} />
-      <h2>Contacts</h2>
-      <Filter/>
-      <ContactList />
-    </div>
-  );
+     <div className={css.container}>
+       <h1>Phonebook</h1>
+
+       <Suspense fallback="Loading...">
+         <Routes>
+           <Route
+             path="/"
+             element={
+               <ContactForm addContactCallback={addContactToPhonebook} />
+             }
+           />
+           <Route path="/contacts" element={<ContactsPage />} />
+         </Routes>
+       </Suspense>
+       <h2>Contacts</h2>
+       <Filter />
+       <ContactList />
+     </div>
+   );
 };
 
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import './index.css';
 
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/goit-react-hw-07-phonebook">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
